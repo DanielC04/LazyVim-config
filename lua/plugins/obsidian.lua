4001dbd80e1807c0a623da63c1fb999181f485a3
Daniel Cermann <daniel.cermann@gmx.de>
2023-09-29 15:38:03 +0200

    templates work at last using vscode-json format

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,6 +1,7 @@
 return {
   "epwalsh/obsidian.nvim",
   lazy = true,
+  log_level = vim.log.levels.DEBUG,
   event = {
     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
@@ -16,6 +17,29 @@ return {
   opts = {
     dir = "/media/daniel/Documents/Obsidian/", -- no need to call 'vim.fn.expand' here
     mappings = {},
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%d-%m-%Y",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%A,  %d %B %Y",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "daily-note.md",
+    },
+    overwrite_mappings = true,
+    templates = {
+      subdir = "templates/",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+      substitutions = {
+        writtendate = function()
+          return os.date("%A, %d. %B %Y")
+        end,
+        yesterday = function()
+          return os.date("%Y-%m-%d", os.time() - 86400)
+        end,
+      },
+    },
   },
-  overwrite_mappings = true,
 }
---
+++ templates/obsidian/daily-note.md
@@ -0,0 +1,12 @@
+---
+id: "{{date}}"
+aliases:
+  - "{{writtendate}}"
+tags:
+  - {{writtendate}}
+  - "daily-notes"
+  - "todos"
+---
+
+# {{writtendate}}
+
