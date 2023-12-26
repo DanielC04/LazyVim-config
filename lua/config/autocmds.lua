84df6179f95f41af97f972ffe114448b211b3079
Daniel Cermann <daniel.cermann@gmx.de>
2023-12-26 11:22:35 +0100

    added basic keymaps and autocommands again

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- automatically format rust code on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.rs" },
+  command = "!/home/daniel/.cargo/bin/rustfmt %",
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,62 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+
+-- set mapleader and localleader
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- open terminal in bottom buffer
+vim.keymap.set("n", "<leader>t", function()
+  if vim.bo.buftype ~= "terminal" then
+    vim.cmd("bo terminal")
+    vim.cmd("horizontal resize 10")
+  else
+    vim.cmd("vsplit")
+    vim.cmd("terminal")
+  end
+  vim.cmd("startinsert")
+end, { desc = "open terminal at bottom" })
+
+-- exit insert/visual mobe using jk instead of <escape>
+vim.keymap.set("i", "jk", "<escape>", { desc = "exit insert mode" })
+vim.keymap.set("v", "jk", "<escape>", { desc = "exit insert mode" })
+vim.keymap.set("t", "jk", "<C-\\><C-N>", { desc = "exit insert mode" })
+
+-- make resizing windows smoother
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("t", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("t", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+
+-- switch spell checked language
+vim.keymap.set(
+  "n",
+  "<leader>spd",
+  "<cmd>set spell spelllang=de_de<cr>",
+  { desc = "set spell checked language to german" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>spe",
+  "<cmd>set spell spelllang=en_us<cr>",
+  { desc = "set spell checked language to english" }
+)
+-- default language: german
+vim.cmd("set spell spelllang=de_de | setlocal nospell ")
+
+-- latex: make rightarrow
+vim.cmd("abb ra $\\rightarrow$")
+
+-- enable gf on links in obsidian
+vim.keymap.set("n", "gf", function()
+  if require("obsidian").util.cursor_on_markdown_link() then
+    return "<cmd>ObsidianFollowLink<CR>"
+  else
+    return "gf"
+  end
+end, { noremap = false, expr = true })
+
+-- Obsidian insert definition
+vim.cmd("abb odef >[!Definition] <CR>>")
