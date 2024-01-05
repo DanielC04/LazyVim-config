f70ff870dc29a4d8fe3a93866765ede0ad009541
Daniel Cermann <daniel.cermann@gmx.de>
2024-01-05 12:42:51 +0100

    can insert new line below with ctrl+Enter now and source config with <leader>sl

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,6 +8,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+-- source neovim config to avoid having to restart neovim everytime I change somehting
+vim.keymap.set("n", "<leader>sv", "<cmd>luafile %<cr>", { desc = "source neovim config" })
+
 -- open terminal in bottom buffer
 vim.keymap.set("n", "<leader>t", function()
   if vim.bo.buftype ~= "terminal" then
@@ -60,3 +63,6 @@ end, { noremap = false, expr = true })
 
 -- Obsidian insert definition
 vim.cmd("abb odef >[!Definition] <CR>>")
+
+-- insert blank line below on control enter
+vim.keymap.set("n", "<C-M>", "<cmd> append <Enter> <CR>", { desc = "insert blank line below" })
